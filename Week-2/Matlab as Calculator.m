63f1f2ccbbf0b248253de6754a9458c4aa5ecafd
Pradumna <65444978+pgautam8601@users.noreply.github.com>
2020-07-19 13:47:40 +0530

    Create Matlab as Calculator.m

---
+++ Week-2/Matlab as Calculator.m
@@ -0,0 +1,8 @@
+money_borrowed=1000;
+interest_rate=.10;
+end_loan=(money_borrowed*interest_rate)+money_borrowed
+%15% of this end lone will be calculated
+new_interest=end_loan*interest_rate
+%it becomes 110
+debt=end_loan+new_interest
+%answer is 1210
